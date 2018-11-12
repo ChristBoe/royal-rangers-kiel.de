e4cb629bff6c4e0b00d6cfb4eecbfacceca0df2b
kirillkiel <kirill.kiel@gmx.net>
2018-11-12 18:09:06 +0100

    erste idee
    idee

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Royal Rangers Kiel</title>
+	<link rel="icon" type="img/png" href="icon.png">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="banner">
+		<img id="logo" src="logo.svg">
+		<div id="willkommen">
+		<h1>Willkommen</h1>
+		<h2>bei den Royal Rangers in Kiel</h2>
+	</div>
+	</div>
+
+</body>
+</html>
