4eb04204dc5a77befe526fb4649f49334eace5e9
kirillkiel <kirill.kiel@gmx.net>
2018-12-02 16:21:53 +0100

    Outsourceing 4 better overwiew

---
+++ inc/mapsswitch.js
@@ -0,0 +1,28 @@
+function switchtohassee()
+		{
+			document.getElementById('tablinkshassee').style.outline = 'none';
+			document.getElementById('elmschenhagen').style.display = 'none';
+			document.getElementById('hassee').style.display = 'block';
+			document.getElementById('tablinkshassee').style.background = '#ccc';
+			document.getElementById('tablinkseh').style.background = '#e0e0e0';
+			document.getElementById('tablinkshassee').style.borderStyle = "inset";
+			document.getElementById('tablinkseh').style.borderStyle = "none";
+			document.getElementById('tablinkseh').style.cursor = "pointer";
+			document.getElementById('tablinkshassee').style.cursor = "default";
+			
+		}
+
+		function switchtoeh()
+		{
+			document.getElementById('tablinkseh').style.outline = 'none';
+			document.getElementById('hassee').style.display = 'none';
+			document.getElementById('elmschenhagen').style.display = 'block';
+			document.getElementById('tablinkseh').style.background = '#ccc';
+			document.getElementById('tablinkshassee').style.background = '#e0e0e0';
+			document.getElementById('tablinkshassee').style.borderStyle = "none";
+			document.getElementById('tablinkseh').style.borderStyle = "inset";
+			document.getElementById('tablinkseh').style.cursor = "default";
+			document.getElementById('tablinkshassee').style.cursor = "pointer";
+		}
+
+		switchtohassee();
