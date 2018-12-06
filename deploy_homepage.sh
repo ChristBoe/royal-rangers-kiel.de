54fa8c0d1dd981128313e59dfe42fff9afb5cf02
Clemens Schmid <clemens@nevrome.de>
2018-12-06 21:52:50 +0100

    Update deploy_homepage.sh

--- deploy_homepage.sh
+++ deploy_homepage.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# install latest curl version
+wget https://curl.haxx.se/download/curl-7.62.0.tar.gz
+tar -xvf curl-7.62.0.tar.gz
+cd curl-7.62.0/
+./configure
+make
+sudo make install
+cd ..
+
 # copy new version to server
 find . -type f -not -path "./.git/*" -not -path "./deploy_homepage.sh" -not -path "./.travis.yml" -exec curl --disable-epsv --retry 5 --retry-connrefused -v -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp.royal-rangers-kiel.de/new_homepage/{} \;
