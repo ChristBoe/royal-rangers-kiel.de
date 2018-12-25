89f5d18ab67fc5e2dc047acae0b1ce5a1750c184
kirillkiel <kirill.kiel@gmx.net>
2018-12-25 22:55:12 +0100

    typo

--- webappinc/addToHStut.js
+++ webappinc/addToHStut.js
@@ -1,6 +1,7 @@
 var chrometut = '<p><h3>Schritt 1</h3> <p>Öffnen Sie das Menü <img src="webappinc/tutpics/chrome/1.png" width="100%"> </p></p>  <br>';
 
-chrometut += '<p><p><h3>Schritt 2</h3><p>Wählen Sie "Zum Startbildschirm hinzufühen" aus<img src="webappinc/tutpics/chrome/2.png"width="100%"></p></p><br>';
+chrometut += '<p><p><h3>Schritt 2</h3><p>Wählen Sie "Zum Startbildschirm hinzufügen" aus<img src="webappinc/tutpics/chrome/2.png"width="100%"></p></p><br>';
+chrometut += '<p><p><h3>Schritt 2</h3><p>Wählen Sie "Zum Startbildschirm hinzufügen" aus<img src="webappinc/tutpics/chrome/2.png"width="100%"></p></p><br>';
 
 chrometut += '<p><p><h3>Schritt 3</h3><p>Bestätigen Sie das Fenster<img src="webappinc/tutpics/chrome/3.png" width="100%"></p></p>';
 
@@ -9,7 +10,7 @@ chrometut += '<p><p><h3>Schritt 3</h3><p>Bestätigen Sie das Fenster<img src="we
 
 var operaminitut = '<p><h3>Schritt 1</h3><p>Öffnen Sie das Menü<img src="webappinc/tutpics/opera/1.png" width="100%"></p></p><br>';
 
-operaminitut += '<p><h3>Schritt 2</h3><p>Wählen Sie "Zum Start-bildschirm hinzufühen" aus<img src="webappinc/tutpics/opera/2.png"width="100%"></p></p><br>';
+operaminitut += '<p><h3>Schritt 2</h3><p>Wählen Sie "Zum Start-bildschirm hinzufügen" aus<img src="webappinc/tutpics/opera/2.png"width="100%"></p></p><br>';
 
 operaminitut += '<p><h3>Schritt 3</h3><p>Bestätigen Sie das Fenster<img src="webappinc/tutpics/opera/3.png" width="100%"></p></p>';
 
@@ -20,7 +21,7 @@ operaminitut += '<p><h3>Schritt 3</h3><p>Bestätigen Sie das Fenster<img src="we
 
 var samsunginternettut = '<p><h3>Schritt 1</h3><p>Öffnen Sie das Menü<img src="webappinc/tutpics/samsungInternet/1.png" width="100%"></p></p><br>';
    
-samsunginternettut += '<p><h3>Schritt 2</h3><p>Wählen Sie "Zum Start-bildschirm hinzufühen" aus<img src="webappinc/tutpics/samsungInternet/2.png" width="100%">s</p></p>';
+samsunginternettut += '<p><h3>Schritt 2</h3><p>Wählen Sie "Zum Start-bildschirm hinzufügen" aus<img src="webappinc/tutpics/samsungInternet/2.png" width="100%">s</p></p>';
 
 
 
