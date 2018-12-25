78400dad1a3c2744c84b257d7d4254cfaf042def
kirillkiel <kirill.kiel@gmx.net>
2018-12-25 18:02:40 +0100

    webapp warscheinlich final

---
+++ webappinc/addToHStut.js
@@ -0,0 +1,24 @@
+var chrometut = '<p><h3>Schritt 1</h3> <p>Öffnen Sie das Menü <img src="webappinc/tutpics/chrome/1.png" width="100%"> </p></p>  <br>';
+
+chrometut += '<p><p><h3>Schritt 2</h3><p>Wählen Sie "Zum Startbildschirm hinzufühen" aus<img src="webappinc/tutpics/chrome/2.png"width="100%"></p></p><br>';
+
+chrometut += '<p><p><h3>Schritt 3</h3><p>Bestätigen Sie das Fenster<img src="webappinc/tutpics/chrome/3.png" width="100%"></p></p>';
+
+
+
+
+var operaminitut = '<p><h3>Schritt 1</h3><p>Öffnen Sie das Menü<img src="webappinc/tutpics/opera/1.png" width="100%"></p></p><br>';
+
+operaminitut += '<p><h3>Schritt 2</h3><p>Wählen Sie "Zum Start-bildschirm hinzufühen" aus<img src="webappinc/tutpics/opera/2.png"width="100%"></p></p><br>';
+
+operaminitut += '<p><h3>Schritt 3</h3><p>Bestätigen Sie das Fenster<img src="webappinc/tutpics/opera/3.png" width="100%"></p></p>';
+
+
+
+
+
+
+var samsunginternettut = '<p><h3>Schritt 1</h3><p>Öffnen Sie das Menü<img src="webappinc/tutpics/samsungInternet/1.png" width="100%"></p></p><br>';
+   
+samsunginternettut += '<p><h3>Schritt 2</h3><p>Wählen Sie "Zum Start-bildschirm hinzufühen" aus<img src="webappinc/tutpics/samsungInternet/2.png" width="100%">s</p></p>';
+   
